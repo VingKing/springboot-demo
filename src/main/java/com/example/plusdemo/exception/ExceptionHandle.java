c8f11c30ba3a98a8d609271cf273cd42bc8e56e3
zhangbin <95983377>
2019-11-07 14:11:03 +0800

    update

--- src/main/java/com/example/plusdemo/exception/ExceptionHandle.java
+++ src/main/java/com/example/plusdemo/exception/ExceptionHandle.java
@@ -3,7 +3,6 @@ package com.example.plusdemo.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
